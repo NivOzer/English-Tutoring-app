c0ac7e9236b441c2231a7613672d673942caa86e
Niv Ozer <niv.ozer@yahoo.com>
2024-03-27 16:49:28 +0200

    Added home screen component

--- App.js
+++ App.js
@@ -7,32 +7,16 @@ import {
   ScrollView,
   Platform,
 } from "react-native";
-import Lesson from "./components/Lesson";
-import logo from "./Logo.png";
-import lessonData from "./lessons.json";
 
-const statusBarHeight = Platform.OS === "ios" ? 50 : StatusBar.currentHeight;
-const paddingTop = statusBarHeight + 0.5 * statusBarHeight;
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+
+import HomeScreen from "./components/HomeScreen";
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Image style={styles.logo} source={logo} resizeMode="contain" />
-      <View style={styles.lessonContainer}>
-        <ScrollView contentContainerStyle={styles.scrollContent}>
-          {lessonData.map((lesson, index) => (
-            <Lesson
-              key={index}
-              name={lesson.name}
-              time={lesson.time}
-              weekDay={lesson.weekDay}
-              phone={lesson.phone}
-              parentPhone={lesson.parentPhone}
-              material={lesson.material}
-            />
-          ))}
-        </ScrollView>
-      </View>
+      <HomeScreen />
     </View>
   );
 }
@@ -40,24 +24,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#eef5ff",
-    alignItems: "center",
-    justifyContent: "center",
-    paddingTop: paddingTop,
-  },
-  logo: {
-    width: "75%",
-    height: "10%",
-    marginBottom: "5%",
-  },
-  lessonContainer: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  scrollContent: {
-    width: "100%",
-    height: "100%",
-    alignItems: "center",
   },
 });
---
+++ components/HomeScreen.tsx
@@ -0,0 +1,68 @@
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  Platform,
+} from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import Lesson from "./Lesson";
+import logo from "../Logo.png";
+import lessonData from "../lessons.json";
+type Props = {}
+const statusBarHeight = Platform.OS === "ios" ? 50 : (StatusBar as any).currentHeight;
+
+const paddingTop = statusBarHeight + 0.5 * statusBarHeight;
+const HomeScreen = (props: Props) => {
+    return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Image style={styles.logo} source={logo} resizeMode="contain" />
+      <View style={styles.lessonContainer}>
+        <ScrollView contentContainerStyle={styles.scrollContent}>
+          {lessonData.map((lesson, index) => (
+            <Lesson
+              key={index}
+              name={lesson.name}
+              time={lesson.time}
+              weekDay={lesson.weekDay}
+              phone={lesson.phone}
+              parentPhone={lesson.parentPhone}
+              material={lesson.material}
+            />
+          ))}
+        </ScrollView>
+      </View>
+    </View>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#eef5ff",
+        alignItems: "center",
+        justifyContent: "center",
+        paddingTop: paddingTop,
+      },
+      logo: {
+        width: "75%",
+        height: "10%",
+        marginBottom: "5%",
+      },
+      lessonContainer: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+      },
+      scrollContent: {
+        width: "100%",
+        height: "100%",
+        alignItems: "center",
+      },
+})
