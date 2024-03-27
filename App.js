84c3fbde3813f11b98de7bd7793c8cac7def2c59
Niv Ozer <niv.ozer@yahoo.com>
2024-03-27 16:52:33 +0200

    Added Edit screen component

--- App.js
+++ App.js
@@ -1,12 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  ScrollView,
-  Platform,
-} from "react-native";
+import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
