21c8edc31b34c53c9b958ce3936f55680cce1e29
Niv Ozer <niv.ozer@yahoo.com>
2024-03-30 21:28:00 +0300

    Added half cluch selct DropDown

--- components/EditScreen.tsx
+++ components/EditScreen.tsx
@@ -2,6 +2,8 @@ import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React, { useState } from "react";
 import Input from "./Input";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import SelectDropdown from "react-native-select-dropdown";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 type LessonData = {
   name: string;
   time: string;
@@ -17,7 +19,9 @@ type Props = {
 
 const EditScreen = ({ addLesson, data }: Props) => {
   console.log(data);
+
   const [showForm, setShowForm] = React.useState(false);
+  const [selectLessonShow, setSelectLessonShow] = React.useState(false);
   const [lessonData, setLessonData] = useState({
     name: "",
     time: "",
@@ -96,10 +100,55 @@ const EditScreen = ({ addLesson, data }: Props) => {
         </>
       )}
 
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => setSelectLessonShow(!selectLessonShow)}
+      >
         <Ionicons name="create" size={60} color="#99cccccc" />
         <Text style={styles.buttonText}>ערוך שיעור:</Text>
       </TouchableOpacity>
+      {selectLessonShow && (
+        <SelectDropdown
+          data={data}
+          onSelect={(selectedItem, index) => {
+            console.log(selectedItem, index);
+          }}
+          renderButton={(selectedItem, isOpened) => {
+            return (
+              <View style={styles.dropdownButtonStyle}>
+                {selectedItem && (
+                  <Icon
+                    name={selectedItem.icon}
+                    style={styles.dropdownButtonIconStyle}
+                  />
+                )}
+                <Icon
+                  name={isOpened ? "chevron-up" : "chevron-down"}
+                  style={styles.dropdownButtonArrowStyle}
+                />
+                <Text style={styles.dropdownButtonTxtStyle}>
+                  {(selectedItem && selectedItem.title) || "בחר שיעור"}
+                </Text>
+              </View>
+            );
+          }}
+          renderItem={(item, index, isSelected) => {
+            return (
+              <View
+                style={{
+                  ...styles.dropdownItemStyle,
+                  ...(isSelected && { backgroundColor: "#D2D9DF" }),
+                }}
+              >
+                <Icon name={item.icon} style={styles.dropdownItemIconStyle} />
+                <Text style={styles.dropdownItemTxtStyle}>{item.title}</Text>
+              </View>
+            );
+          }}
+          showsVerticalScrollIndicator={false}
+          dropdownStyle={styles.dropdownMenuStyle}
+        />
+      )}
     </View>
   );
 };
@@ -143,5 +192,51 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 20,
     color: "white",
+  },
+  // Select DropDown
+  dropdownButtonStyle: {
+    width: 200,
+    height: 50,
+    backgroundColor: "#E9ECEF",
+    borderRadius: 12,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 12,
+  },
+  dropdownButtonTxtStyle: {
+    flex: 1,
+    fontSize: 18,
+    fontWeight: "500",
+    color: "#151E26",
+  },
+  dropdownButtonArrowStyle: {
+    fontSize: 28,
+  },
+  dropdownButtonIconStyle: {
+    fontSize: 28,
+    marginRight: 8,
+  },
+  dropdownMenuStyle: {
+    backgroundColor: "#E9ECEF",
+    borderRadius: 8,
+  },
+  dropdownItemStyle: {
+    width: "100%",
+    flexDirection: "row",
+    paddingHorizontal: 12,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 8,
+  },
+  dropdownItemTxtStyle: {
+    flex: 1,
+    fontSize: 18,
+    fontWeight: "500",
+    color: "#151E26",
+  },
+  dropdownItemIconStyle: {
+    fontSize: 28,
+    marginRight: 8,
   },
 });
